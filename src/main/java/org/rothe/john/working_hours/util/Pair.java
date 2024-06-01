e7f9941b87547e8db3716086920ecccff4d4a720
jdr-daugherty <john.rothe@daugherty.com>
2024-06-01 14:47:21 -0400

    minor efficiency tweaks

--- src/main/java/org/rothe/john/working_hours/util/Pair.java
+++ src/main/java/org/rothe/john/working_hours/util/Pair.java
@@ -4,8 +4,12 @@ import lombok.val;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+
+import static java.util.Spliterator.*;
 
 public class Pair<T> {
     private final T left;
@@ -16,17 +20,21 @@ public class Pair<T> {
         this.right = right;
     }
 
-    public static <T> Stream<Pair<T>> stream(List<T> list) {
-        Iterable<Pair<T>> iterable = () -> new PairingIterator<>(list);
-        return StreamSupport.stream(iterable.spliterator(), false);
-    }
-
     public T left() {
         return left;
     }
 
     public T right() {
         return right;
+    }
+
+    public static <T> Stream<Pair<T>> stream(List<T> list) {
+        return StreamSupport.stream(spliterator(list), false);
+    }
+
+    private static <T> Spliterator<Pair<T>> spliterator(List<T> list) {
+        return Spliterators.spliterator(new PairingIterator<>(list),
+                list.size(), SIZED | IMMUTABLE | NONNULL | ORDERED);
     }
 
     private static class PairingIterator<T> implements Iterator<Pair<T>> {
