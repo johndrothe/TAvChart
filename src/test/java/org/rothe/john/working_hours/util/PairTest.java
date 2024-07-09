5cc94f04ae8f779a043de3d1fd61877a97e601fe
jdr-daugherty <jdr-daugherty@gmail.com>
2024-07-08 21:30:41 -0400

    additional unit testing

---
+++ src/test/java/org/rothe/john/working_hours/util/PairTest.java
@@ -0,0 +1,28 @@
+package org.rothe.john.working_hours.util;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PairTest {
+
+    @Test
+    void testLeftRight() {
+        Pair<String> pair = new Pair<>("left", "right");
+        assertEquals("left", pair.left());
+        assertEquals("right", pair.right());
+    }
+
+    @Test
+    void testStreamSimpleSets() {
+        val list = List.of("a", "b", "c", "d", "e");
+        assertEquals(Set.of("a", "b", "c", "d"), Pair.stream(list).map(Pair::left).collect(Collectors.toSet()));
+        assertEquals(Set.of("b", "c", "d", "e"), Pair.stream(list).map(Pair::right).collect(Collectors.toSet()));
+    }
+
+}
