8e36a6c24093aa5ad46c06fe35672d0614e46647
johndrothe <johndrothe@gmail.com>
2024-07-12 20:31:02 -0400

    additional testing for Pair, its split iterator, and its stream handling

--- src/main/java/org/rothe/john/working_hours/util/Pair.java
+++ src/main/java/org/rothe/john/working_hours/util/Pair.java
@@ -5,21 +5,29 @@ import lombok.val;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static java.util.Spliterator.*;
+import static java.util.Spliterator.IMMUTABLE;
+import static java.util.Spliterator.NONNULL;
+import static java.util.Spliterator.ORDERED;
+import static java.util.Spliterator.SIZED;
 
 @EqualsAndHashCode
 public class Pair<T> {
     private final T left;
     private final T right;
 
-    Pair(T left, T right) {
-        this.left = left;
-        this.right = right;
+    private Pair(T left, T right) {
+        this.left = Objects.requireNonNull(left);
+        this.right = Objects.requireNonNull(right);
+    }
+
+    public static <T> Pair<T> of(T left, T right) {
+        return new Pair<>(left, right);
     }
 
     public T left() {
@@ -34,6 +42,11 @@ public class Pair<T> {
         return StreamSupport.stream(spliterator(list), false);
     }
 
+    @Override
+    public String toString() {
+        return String.format("Pair(%s, %s)", left, right);
+    }
+
     private static <T> Spliterator<Pair<T>> spliterator(List<T> list) {
         return Spliterators.spliterator(new PairingIterator<>(list),
                 Math.max(0, list.size() - 1), SIZED | IMMUTABLE | NONNULL | ORDERED);
@@ -41,19 +54,19 @@ public class Pair<T> {
 
     private static class PairingIterator<T> implements Iterator<Pair<T>> {
         private final Iterator<T> it;
-        private T left = null;
+        private T nextLeft = null;
 
         PairingIterator(List<T> list) {
             this.it = List.copyOf(list).iterator();
             if (it.hasNext()) {
-                this.left = it.next();
+                this.nextLeft = it.next();
             }
         }
 
         @Override
         public Pair<T> next() {
-            val pair = new Pair<>(left, it.next());
-            left = pair.right();
+            val pair = Pair.of(nextLeft, it.next());
+            nextLeft = pair.right();
             return pair;
         }
 
--- src/test/java/org/rothe/john/working_hours/util/PairTest.java
+++ src/test/java/org/rothe/john/working_hours/util/PairTest.java
@@ -8,37 +8,67 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PairTest {
 
     @Test
     void testLeftRight() {
-        Pair<String> pair = new Pair<>("left", "right");
+        Pair<String> pair = Pair.of("left", "right");
         assertEquals("left", pair.left());
         assertEquals("right", pair.right());
     }
 
     @Test
-    void testStreamSimpleSets() {
+    void testNullPairsThrowNPE() {
+        assertThrows(NullPointerException.class, () -> Pair.of(null, "right"));
+        assertThrows(NullPointerException.class, () -> Pair.of("left", null));
+    }
+
+    @Test
+    void testToString() {
+        Pair<String> pair = Pair.of("a", "b");
+        assertEquals("Pair(a, b)", pair.toString());
+    }
+
+    @Test
+    void testPairStreamFromEmptyIsEmpty() {
+        assertTrue(Pair.stream(List.of()).toList().isEmpty());
+    }
+
+    @Test
+    void testPairStreamFromSingleEntryIsEmpty() {
+        assertTrue(Pair.stream(List.of("a")).toList().isEmpty());
+    }
+
+    @Test
+    void testPairStreamSimpleSets() {
         val list = List.of("a", "b", "c", "d", "e");
         assertEquals(Set.of("a", "b", "c", "d"), Pair.stream(list).map(Pair::left).collect(Collectors.toSet()));
         assertEquals(Set.of("b", "c", "d", "e"), Pair.stream(list).map(Pair::right).collect(Collectors.toSet()));
     }
 
     @Test
-    void testPairStreamToList() {
+    void testOddLengthPairStreamToList() {
         val expected = List.of(
-                new Pair<>("a", "b"),
-                new Pair<>("b", "c"),
-                new Pair<>("c", "d"),
-                new Pair<>("d", "e")
+                Pair.of("a", "b"),
+                Pair.of("b", "c"),
+                Pair.of("c", "d"),
+                Pair.of("d", "e")
         );
         val result = Pair.stream(List.of("a", "b", "c", "d", "e")).toList();
         assertEquals(expected, result);
     }
+
     @Test
-    void testEmptyPairStream() {
-        assertTrue(Pair.stream(List.of()).toList().isEmpty());
+    void testEvenLengthPairStreamToList() {
+        val expected = List.of(
+                Pair.of("a", "b"),
+                Pair.of("b", "c"),
+                Pair.of("c", "d")
+        );
+        val result = Pair.stream(List.of("a", "b", "c", "d")).toList();
+        assertEquals(expected, result);
     }
 }
